cb4b29e110f66ff78398f5045bd97f8fad1a6362
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-03-05 18:49:58 -0300

    gerenciamento dos usuários - listar e deletar os usuários

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+  	@users = User.all
+  end
+
+  def destroy
+  	User.destroy(params[:id].to_i)
+  	respond_to do |format|
+      format.html { redirect_to configuracao_path, notice: 'Usuário deletado com sucesso.' }
+      format.json { head :no_content }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :registros
   resources :associados
+  resources :home
+
+  match 'configuracao', :to => 'home#index', via: 'get'
+
+
+  match 'rack',
+   :to => proc{|env| [200, {"Content-Type" => "text/html"}, 
+  ["App Rack numa rota Rails"]]}, via:'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
